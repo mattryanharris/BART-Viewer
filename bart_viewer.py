ce5299d701eb00966d7354bcfa8c3546631a0539
Matthew Harris <mrharris@jpl.nasa.gov>
2020-11-17 01:26:51 -0800

    Updated Python script to support catching times when the BART is either down or not sending updates regarding their trains

--- bart_viewer.py
+++ bart_viewer.py
@@ -5,7 +5,11 @@ station = raw_input("What station: ")
 response = requests.get("http://api.bart.gov/api/etd.aspx?cmd=etd&key=MW9S-E7SL-26DU-VV8V&orig=%s&json=y" % station)
 data = response.json()
 for x in xrange(0,2):
-	name = data['root']['station'][0]['etd'][x]['destination']
+	try:
+		name = data['root']['station'][0]['etd'][x]['destination']
+	except KeyError:
+		print("Error: {}").format(data['root']['station'][0]['message']['error'])
+		break
 	time = data['root']['station'][0]['etd'][x]['estimate'][0]['minutes']
 	if name == 'Leaving':
 		print('The ' + name + ' train is leaving now')
--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
 
+function Welcome(props) {
+    return <h3>Hello, {props.name}</h3>;
+  }
+
 function App() {
 	return (
-        <div id="root"></div>
+        <div id="root">
+            <div>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
+      <Welcome name="Teddie" />
+      <h1>Test</h1>
+    </div>
+        </div>
     );
 }
+
 
 export default App;
